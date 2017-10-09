23c707afa1671797df514d45decf6e640fb15209
leeminam32 <leeminam32@gmail.com>
2017-10-10 00:10:55 +0900

    It's too hard

--- src/commands.c
+++ src/commands.c
@@ -1,30 +1,51 @@
 #include <string.h>
-
+#include <stdio.h>
 #include "commands.h"
+#include <stdlib.h>
+#include <dirent.h>
+#include <unistd.h>
 
 int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
     return -1;
-
-  // TODO: Fill it!
+  if (argc == 1)
+	chdir(getenv("HOME"));
+  else if (argc == 2) {
+	if(chdir(argv[1]))
+		printf("There is No directory"); 
+	}
+//  else if (argc > 2) printf("Wrong input");
+	
   return 0;
 }
 
 int do_pwd(int argc, char** argv) {
   if (!validate_pwd_argv(argc, argv))
     return -1;
-
-  // TODO: Fill it!
-
+  char buff[8096];
+	
+	getcwd(buff, 8096);
+	printf("%s\n",buff);
+	
   return 0;
 }
 
 int validate_cd_argv(int argc, char** argv) {
-  // TODO: Fill it!
+  if(argc >2 || argc == 0){
+  printf("wrong input");
+	return 0;
+}
+  if(strcmp(argv[0], "cd") != 0) return 0;
+
   return 1;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
-  // TODO: Fill it!
+
+  if(argc != 1)
+	 return 0;
+  if(strcmp(argv[0], "pwd") != 0) return 0;
+  
+
   return 1;
 }
--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,37 @@
 #include "utils.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
-void mysh_parse_command(const char* command,
-                        int *argc, char*** argv)
+void mysh_parse_command(const char *command, int *argc, char ***argv)
 {
-  // TODO: Fill this!
+
+  int num = 0;
+  char*copycmd = (char*)malloc(sizeof(char)*8096);
+  char*temp = (char*)malloc(sizeof(char)*8096);
+
+	(*argv) = (char **) malloc(sizeof(char*)*8096);
+	for(int i =0; i< 8096; i++){
+	  *(*argv+i) = (char*) malloc(sizeof(char)*8096);
+}
+
+
+	strcpy(copycmd,command);
+	
+	temp = strtok(copycmd," \n\t"); 
+	
+  if(temp == NULL){
+	strcpy(*(*argv+num),"");
+	num++;
+}
+
+  while (temp != NULL){
+	
+	strcpy(*(*argv+num),temp);
+	temp = strtok(NULL," \t\n");
+	num++;	
+ }
+//	free(copycmd);
+//	free(temp);
+	
 }
